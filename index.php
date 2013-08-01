1983451ee444872e1fc51eb905729fc8a66626f6
David Chang <dchang.me@gmail.com>
2013-08-01 08:21:17 -0700

    stripping many things down, still havent made a page yet though

--- index.php
+++ index.php
@@ -13,65 +13,67 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
+?>
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<link href='http://fonts.googleapis.com/css?family=Didact+Gothic' rel='stylesheet' type='text/css'>
+<?php wp_head(); ?>
+</head>
 
-get_header(); ?>
+<body <?php body_class(); ?>>
+<div id='mainContainer'>
+	<header>
+		<h1>Chinese Southern Baptist Church</h1>
+		<h2>English Ministry</h2>
+	</header>
 
-	<div id="primary">
-		<div id="content" role="main">
-			<section>
-				<div id='carousel'>
-					<a href='#/carousel1'><div id='slide1' class='slide'></div></a>
-				</div>
-			</section>
-			
-			<section id='latestMessageSection' class='clear cap'>
-				<div id='latestMessage' class='left'>
-					<div>Latest</div>
-					<div>Message</div>
-				</div>
-				<div id='date' class='left'>
-					<div>May</div>
-					<div>26</div>
-				</div>
-				<div id='sermonTitle' class='left'>
-					<div>How to be a Man - Part One</div>
-					<div>Pastor Matt Zwitt</div>
-				</div>
-				<div id='slides' class='right button'>
-					Slides
-				</div>
-				<div id='download' class='right button'>
-					Download
-				</div>
-				<div id='listen' class='right button'>
-					Listen
-				</div>
-			</section>
-			
-			<section id='boxes' class='clear'>
-				<div id='resources' class='boxDiv'>
-					<a href='resources'>
-						<div class='box'></div>
-					</a>
-				</div>
-				<div id='events' class='boxDiv'>
-					<a href='events'>
-						<div class='box'></div>
-					</a>
-				</div>
-				<div id='location' class='boxDiv'>
-					<a href='location'>
-						<div class='box'></div>
-					</a>
-				</div>
-				<div id='contact' class='boxDiv'>
-					<a href='contact'>
-						<div class='box'></div>
-					</a>
-				</div>
-			</section>
+	<div id="main" class="wrapper">
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+		<div id="primary">
+			<div id="content" role="main">
+				
+				<section id='boxes' class='clear'>
+					<div id='resources' class='boxDiv'>
+						<a href='resources'>
+							<div class='box'></div>
+						</a>
+					</div>
+					<div id='events' class='boxDiv'>
+						<a href='events'>
+							<div class='box'></div>
+						</a>
+					</div>
+					<div id='location' class='boxDiv'>
+						<a href='location'>
+							<div class='box'></div>
+						</a>
+					</div>
+					<div id='contact' class='boxDiv'>
+						<a href='contact'>
+							<div class='box'></div>
+						</a>
+					</div>
+				</section>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
 
 <?php get_footer(); ?>
